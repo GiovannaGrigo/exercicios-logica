f254b16d41369848160f2119bd12ad8b48496a01
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-06 14:20:53 -0300

    Exercício 2

---
+++ Ex2.js
@@ -0,0 +1,9 @@
+// Exercício 2: José quer viajar para os Estados Unidos e não sabe quantos dólares ele terá se comprar com o dinheiro em real que ele tem guardado.
+
+var real;
+var dolar;
+
+real = prompt("Quantos reais você tem?");
+
+dolar = real * 5.57;
+console.log(`Você terá ${dolar} dólares!`);
