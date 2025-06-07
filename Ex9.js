1f362800130bf62cc5527a804a5cd68729162b02
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-07 10:47:19 -0300

    Exercício 9

---
+++ Ex9.js
@@ -0,0 +1,12 @@
+// Descubra a média de um aluno e se ele está APROVADO ou REPROVADO. Considere que a nota minima para ser aprovado é 7, e que o aluno tem somente duas notas
+
+var nota1 = prompt("Qual a sua primeira nota?");
+var nota2 = prompt("Qual a sua segunda nota?");
+
+var media = (parseInt(nota1) + parseInt(nota2)) / 2;
+
+if (media >= 7) {
+    console.log(`A sua média é ${media} e você está aprovado!`);
+} else {
+    console.log(`A sua média é ${media} e você está reprovado!`);
+}
