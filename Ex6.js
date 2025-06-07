430da51ad62f1ecf67a13c655344cd844b77480e
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-07 09:58:30 -0300

    Exercício 6

---
+++ Ex6.js
@@ -0,0 +1,9 @@
+// Descubra se um número é par ou ímpar
+
+var num = prompt("Digite um número.");
+
+if (num % 2 == 0) {
+    console.log(`O número ${num} é par!`);
+} else {
+    console.log(`O número ${num} é ímpar!`);
+}
