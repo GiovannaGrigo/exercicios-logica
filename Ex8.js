cff56fd25a9aa9170b7537a0edb53471d75e1472
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-07 10:19:49 -0300

    Exercício 8

---
+++ Ex8.js
@@ -0,0 +1,9 @@
+// Descubra se uma pessoa está apta para dirigir
+
+var idade = prompt("Quantos anos você tem?");
+
+if (idade < 18) {
+    console.log("Você ainda não pode dirigir. Espere até os 18 anos!");
+} else {
+    console.log("Você já pode dirigir. Atente-se ao trânsito!");
+}
