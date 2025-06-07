db5b960fee33f16658757c914312a87ba24d26a1
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-07 09:23:37 -0300

    Exercício 3

---
+++ Ex3.js
@@ -0,0 +1,10 @@
+// Conversor de temperatura: Celsius para Fahrenheit
+
+var grausC;
+var grausF;
+
+grausC = prompt("Quantos graus Celsius está fazendo agora?");
+
+grausF = (grausC - 32) / 1.28
+
+console.log(`${grausC}°C é ${grausF}°F.`);
