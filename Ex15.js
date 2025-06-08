66b4edf35bd30e260b5db838bd4fbd4a67f3a245
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-08 10:04:51 -0300

    Exercício 15

---
+++ Ex15.js
@@ -0,0 +1,13 @@
+// // Contador de 0 a um número pedido ao usuário mas dessa vez, somar esses números. Usando doWhile
+
+let soma = 0;
+let contador = 0;
+let resposta = "S"
+
+do {
+    let num = parseInt(prompt("Digite um número: "));
+    soma += num;
+
+    resposta = prompt("Quer continuar? [S/N]");
+} while (resposta == "S");
+console.log("A soma de todos os valores digitados é " + soma);
