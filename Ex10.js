7f5b106e43397261208ec63c0ba50610425daec7
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-08 09:18:48 -0300

    Exercícios 10 e 11

---
+++ Ex10.js
@@ -0,0 +1,8 @@
+// Contador de 0 a 10 usando laço de repetição While
+
+let contador = 0;
+
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
---
+++ Ex11.js
@@ -0,0 +1,8 @@
+// Contador de 10 a 0 mostrando apenas números pares usando alço de repetição While
+
+let contador = 10;
+
+while (contador >= 0) {
+    console.log(contador);
+    contador -= 2;
+}
