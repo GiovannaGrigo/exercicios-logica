cac5275865ba15dee58f49b9e02415775bf4ff46
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-08 09:45:41 -0300

    Exercício 14 - Melhoria: Adiciona a possibilidade do usuário escolher quantas vezes será convertido

--- Ex14.js
+++ Ex14.js
@@ -1,10 +1,12 @@
 // Contador que faça a conversão de real para dólar 4 vezes
+// Agora, faça com que o usuário decida quantas vezes ele poderá fazer a conversão
 
 let cotacaoDolar = parseFloat(prompt("Informe a cotação do dólar (ex: 5.20):"));
+let qtdeConversao = parseInt(prompt("Quantas vezes você quer fazer a conversão?"));
 let contador = 1;
 
-while (contador <= 4) {
-    let valorReais = parseFloat(prompt(`(${contador}/4) Digite um valor em reais:`));
+while (contador <= qtdeConversao) {
+    let valorReais = parseFloat(prompt(`(${contador}/${qtdeConversao}) Digite um valor em reais:`));
 
     let valorDolares = valorReais / cotacaoDolar;
     console.log(`R$${valorReais.toFixed(2)} = US$${valorDolares.toFixed(2)}`);
