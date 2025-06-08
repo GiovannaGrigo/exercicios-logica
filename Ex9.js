2fc3e2d8886dfb05a2177ef0799db20d6284d4df
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-07 22:22:01 -0300

    Alteração no Exercício 9
    Adiciona a possibilidade do aluno estar em recuperação.

--- Ex9.js
+++ Ex9.js
@@ -1,4 +1,7 @@
-// Descubra a média de um aluno e se ele está APROVADO ou REPROVADO. Considere que a nota minima para ser aprovado é 7, e que o aluno tem somente duas notas
+// Descubra a média de um aluno e se ele está APROVADO ou REPROVADO. Considere que:
+// A nota minima para ser aprovado é 7,
+// O aluno tem apenas 2 notas,
+// Se a nota for entre 4 e 6, então ele está de RECUPERAÇÃO.
 
 var nota1 = parseFloat(prompt("Qual a sua primeira nota?"));
 var nota2 = parseFloat(prompt("Qual a sua segunda nota?"));
@@ -6,7 +9,18 @@ var nota2 = parseFloat(prompt("Qual a sua segunda nota?"));
 var media = (nota1 + nota2) / 2;
 
 if (media >= 7) {
-    console.log(`A sua média é ${media} e você está aprovado!`);
+    console.log(`A sua média é ${media}, e você está APROVADO!`);
+} else if (media >= 4) {
+    console.log(`A sua média é ${media}, e você está de RECUPERAÇÃO!`);
 } else {
-    console.log(`A sua média é ${media} e você está reprovado!`);
+    console.log(`A sua média é ${media}, e você está REPROVADO!`);
 }
+
+
+/* CÓDIGO ANTERIOR
+ if (media >= 7) {
+    console.log(`A sua média é ${media} e você está aprovado!`);
+ } else {
+    console.log(`A sua média é ${media} e você está reprovado!`);
+ }
+*/
