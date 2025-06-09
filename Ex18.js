3859eb8000d20a2b97a8d9366211ca227cef8575
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-08 21:38:35 -0300

    Exercício 18 - Melhoria
    Adiciona a possibilidade do usuário continuar vendo o fatorial de um número quantas vezes quiser.

--- Ex18.js
+++ Ex18.js
@@ -1,11 +1,41 @@
 // Cálculo de fatorial
+// Agora adiciona a possibilidade do usuário ver o fatorial de um número quantas vezes quiser
 
+let resposta;
+
+do {
+    let num = parseInt(prompt("Digite um número: "));
+    let fatorial = 1;
+    let contador = 1;
+
+    do {
+        fatorial *= contador;
+        contador++;
+    } while (contador <= num);
+    console.log(`O fatorial de ${num} é ${fatorial}`);
+
+    resposta = prompt("Quer continuar? [S/N]");
+} while (resposta === "S");
+
+
+
+
+// CÓDIGO SEM PERGUNTAR SE O USUÁRIO DESEJA CONTINUAR
+/*
 let num = parseInt(prompt("Digite um número: "));
 let fatorial = 1;
-contador = 1;
+let contador = 1;
 
 do {
     fatorial *= contador;
     contador++;
+    resposta = prompt("Quer continuar? [S/N]");
+    if (resposta == "S") {
+        do {
+            fatorial *= contador;
+            contador++;
+        } while (resposta != "N");
+    }
 } while (contador <= num);
 console.log(`O fatorial de ${num} é ${fatorial}`);
+*/
