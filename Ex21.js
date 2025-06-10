ef46066020449f955f07c294266f9510aaf1301e
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-10 09:30:40 -0300

    Exercício 21

---
+++ Ex21.js
@@ -0,0 +1,17 @@
+// Contador de 0 a 10 usando o laço de repetição For
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+
+// Contador de 0 a 10 usando o laço de repetição For, dessa vez contando de 2 em 2
+for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+
+
+// Contador de 10 a 0 usando o laço de repetição For
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
