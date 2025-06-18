1fe9041240ee6fe791980ed4c893e0341ffbe7af
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-18 14:50:50 -0300

    Exercício 28

---
+++ Ex28.js
@@ -0,0 +1,7 @@
+// Escreva um programa que utilize dois laços de repetição aninhados (um dentro do outro) para exibir todos os pares possíveis de números entre 1 e 3
+
+for (let c1 = 1; c1 <= 3; c1++) {
+    for (let c2 = 1; c2 <= 3; c2++) {
+        console.log(c1, c2);
+    }
+}
