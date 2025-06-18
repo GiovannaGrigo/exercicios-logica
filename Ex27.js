01985be9257a99d21689101a164f169d3609fb99
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-18 14:38:30 -0300

    Exercício 27 - Adicional
    Agora o código mostra a soma dos números ímpares entre 0 e 10.

--- Ex27.js
+++ Ex27.js
@@ -9,3 +9,18 @@ for (let i = 1; i <= qtde; i++) {
         console.log(`O número ${num} está entre 0 e 10 (posição ${i})`);
     }
 }
+
+
+// Agora, exibir a soma dos números ímpares digitados que estão entre 0 e 10
+
+var qtde = 5;
+var num = 0;
+var soma = 0;
+
+for (let i = 1; i <= qtde; i++) {
+    num = parseFloat(prompt(`Digite o ${i}º número: `));
+    if (num > 0 && num < 10 && num % 2 != 0) {
+        soma += num;
+    }
+}
+console.log(soma);
